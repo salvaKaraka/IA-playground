91c46f843c8a880edfeb50a883f7e13fd361a363
Salvador <salvador.karakachoff@gmail.com>
2023-06-06 14:11:28 -0300

    my first neuron (i think)

--- First contact ever/first-contact.py
+++ First contact ever/first-contact.py
@@ -14,29 +14,30 @@ train_data = [
 train_count = len(train_data)
 
 
-def cost(random_entry):
-    result = 0
+def cost(training_value):
+    cost_result = 0
     for i in range(0, train_count):
         train_entry = train_data[i][0]
         expected = train_data[i][1]
-        try_result = train_entry * random_entry
+        try_result = train_entry * training_value
         delta_error = try_result - expected
-        result += delta_error*delta_error
-        print("Cost:", cost(random_entry),"random_entry:", random_entry)
-    result /= train_count
-    return result
+        cost_result += delta_error*delta_error
+    cost_result /= train_count
+    return cost_result
 
-random_entry=random.random() *10.0 #we generate a random value from zero to one            
+training_value=random.random() *10.0 #we generate a random value from zero to one            
 
 eps = 1e-3
 rate = 1e-3
 
 for i in range(0, 1000):
-    dcost = (cost(random_entry+eps) - cost(random_entry))/eps
-    random_entry -= rate*dcost
+    dcost = (cost(training_value+eps) - cost(training_value))/eps
+    training_value -= rate*dcost
+    print("Cost:", cost(training_value),"trained_value:", training_value)
 
-print("Result:", random_entry)
+print("enter a number:")
+user_entry = int(input())
 
-print("for entry value 5, the result is:", random_entry*5)
+print("for entry value ",user_entry,", the result is (aprox):", training_value*user_entry)
 
 
